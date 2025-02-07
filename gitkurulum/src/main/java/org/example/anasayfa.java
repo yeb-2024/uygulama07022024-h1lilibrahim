32f2f6f3cf0aa973483a570d08ff93e8ff2c04e9
Halil İbrahim Köklü <halilibrahimm3446@gmail.com>
2025-02-07 18:36:47 +0300

    .

--- gitkurulum/src/main/java/org/example/anasayfa.java
+++ gitkurulum/src/main/java/org/example/anasayfa.java
@@ -1,15 +1,28 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class anasayfa {
+public class anasayfa implements ActionListener {
     JFrame frame = new  JFrame();
+    JButton button = new JButton("profili böster");
+
 
     public  anasayfa(){
+        button.addActionListener(this);
+        button.setBounds(200,200,200,200);
+        frame.add(button);
+
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("anasayfa");
         frame.setLayout(null);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
