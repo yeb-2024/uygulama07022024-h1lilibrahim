08bb55ec9c1fd5a5cb94f973f00ebe7a16d97de5
Halil İbrahim Köklü <halilibrahimm3446@gmail.com>
2025-02-07 18:49:38 +0300

    label

--- gitkurulum/src/main/java/org/example/profilsayfasi.java
+++ gitkurulum/src/main/java/org/example/profilsayfasi.java
@@ -1,16 +1,20 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class profilsayfasi {
 
     JFrame frame = new  JFrame();
-
+    JLabel label = new JLabel();
     public profilsayfasi(){
+        label.setText("isim fsm");
+        label.setFont(new Font(null,Font.PLAIN,25));
+        frame.add(label);
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("anasayfa");
-        frame.setLayout(null);
+        frame.setLayout(new GridLayout(3,1));
         frame.setVisible(true);
     }
 }
