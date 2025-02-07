e49413c7767b79706d1179228364cc5072fb9e5c
Halil İbrahim Köklü <halilibrahimm3446@gmail.com>
2025-02-07 18:29:48 +0300

    .

---
+++ gitkurulum/src/main/java/org/example/anasayfa.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class anasayfa {
+    JFrame frame = new  JFrame();
+
+    public  anasayfa(){
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("anasayfa");
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
